175a0da0451fc84e8341a69ecbb6a8fa7f8e9563
erackley <erackley@malwarebytes.org>
2017-01-30 13:55:10 -0500

    * Added missing items!

---
+++ Assets/GridsInteractiveArt/Scripts/ServerPlayerSpawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class ServerPlayerSpawner : NetworkBehaviour {
+
+    [SerializeField]
+    public GameObject NetworkPlayerPrefab;
+    public int numberOfBotsToSpawn = 1;
+
+    [ServerCallback]
+    void Start()
+    {
+        for (int i = 0; i < numberOfBotsToSpawn; i++)
+        {
+            Vector3 startPos = new Vector3(0f, 0f, 0f);
+            Quaternion startRotation = new Quaternion();
+            GameObject obj = Instantiate(NetworkPlayerPrefab, startPos, startRotation);
+            obj.GetComponent<NetworkIdentity>().localPlayerAuthority = false;
+            
+            obj.tag = "Bot";
+            obj.name = "Bot" + i.ToString();
+            NetworkServer.Spawn(obj);
+        }
+    }
+}
