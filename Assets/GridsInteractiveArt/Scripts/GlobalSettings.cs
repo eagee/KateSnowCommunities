7b392f0abe7a0bad1e2b93ea97406965168ca34a
erackley <erackley@malwarebytes.org>
2019-08-30 12:36:43 -0400

    Added missing GlobalSettings file.

---
+++ Assets/GridsInteractiveArt/Scripts/GlobalSettings.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public sealed class GlobalSettings
+{
+    private static readonly GlobalSettings instance = new GlobalSettings();
+
+    private bool m_showPalette = true;
+    private bool m_allowToBeServer = false;
+    
+    private GlobalSettings() { }
+
+    public static GlobalSettings Instance
+    {
+        get
+        {
+            return instance;
+        }
+    }
+
+    public bool ShowPalette
+    {
+        get { return m_showPalette; }
+    }
+    public bool AllowToBeServer
+    {
+        get { return m_allowToBeServer; }
+    }
+
+
+}
